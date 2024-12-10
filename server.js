52a5de6e1aa39a47c58506990d2238511e3d68bd
Cesar Preza <cpreza93@yahoo.com>
2024-12-09 19:20:12 -0800

    installed express and ejs

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const dotenv = require("dotenv")
+dotenv.config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+const Fruit = require('./models/fruit.js');
 
 app.get('/', async (req, res) => {
     res.render('index.ejs')
